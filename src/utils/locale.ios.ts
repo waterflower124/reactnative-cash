90aa3a06965b81d49fd38b5e3099195bbfdd7bdb
waterflower124 <waterflower12591@gmail.com>
2019-10-02 17:50:27 +0800

    changes done to solve error ios 13

--- src/utils/locale.ios.ts
+++ src/utils/locale.ios.ts
@@ -1,5 +1,21 @@
 import { NativeModules } from 'react-native'
 
-export const _locale = NativeModules.SettingsManager.settings.AppleLocale
+// export const _locale = NativeModules.SettingsManager.settings.AppleLocale
+export const getLocale = () => {
+    let _locale = NativeModules.SettingsManager.settings.AppleLocale;
+    if (_locale === undefined) {
+        // iOS 13 workaround, take first of AppleLanguages array  ["en", "en-NZ"]
+        _locale = NativeModules.SettingsManager.settings.AppleLanguages[0]
+        if (_locale == undefined) {
+            _locale = "en" // default language
+        }
+    }
+    return _locale;
+}
+
+export const _locale = getLocale();
+console.log("_locale", _locale);
 export const [lang, region] = _locale.split('_', 2)
 export const locale = `${lang}-${region}`
+
+
